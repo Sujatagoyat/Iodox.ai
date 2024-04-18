c4c865bd21fcf57ed27cea0e8497bd3dd10d0cf2
Sujatagoyat <sujatagoyat8@gmail.com>
2024-04-18 13:49:29 +0530

    slider sec done

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,3 +20,53 @@ $('.parentBox').slick({
   pauseOnHover: true,
   cssEase: 'linear',
 });
+
+$('.responsive').slick({
+  dots: true,
+  infinite: true,
+  centerMode:true,
+  CenterPadding:"60px",
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode:false,
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode:false,
+        CenterPadding:"60px",
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+//      Accordion
+const accordionHead = document.querySelectorAll(".accordionHead");
+accordionHead.forEach((e) => {
+    e.addEventListener("click", () => {
+        const activeAccordion = document.querySelector(".active")
+        e.parentElement.classList.toggle("active")
+        activeAccordion && activeAccordion.classList.remove("active")
+    })
+})
